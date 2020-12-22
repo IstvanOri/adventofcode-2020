043c963ee21a47f65523348728168c03d30dc2e1
Istvan Ori <iori@chemaxon.com>
2020-12-22 21:44:03 +0100

    Day21-Day22 Draft

---
+++ ac2020/days/Day21.py
@@ -0,0 +1,66 @@
+from collections import defaultdict
+
+from ac2020.days import AbstractDay
+from ac2020.io import InputReader
+
+
+class Day21(AbstractDay.AbstractDay):
+
+    def _set_input(self, input_: str):
+        self.entries = {}
+        self.ingredients = []
+        self.foods = []
+        self.allergens = []
+        if len(input_) != 0:
+            lines = InputReader.read_line_by_line(input_)
+            for line in lines:
+                split = line.split(' (')
+                self.foods.append(split[0].split())
+                for x in split[0].split():
+                    self.ingredients.append(x)
+                self.allergens.append(split[1].replace(')', '').replace('contains ', '').split(', '))
+
+    def part1(self):
+        possibilities = defaultdict(set)
+        dangerous = {}
+        allergen_foods = set()
+        for i in range(len(self.foods)):
+            for allergen in self.allergens[i]:
+                if len(possibilities[allergen]) == 0:
+                    possibilities[allergen] = set(self.foods[i])
+                else:
+                    possibilities[allergen] = possibilities[allergen].intersection(set(self.foods[i]))
+        for allergen_food in allergen_foods:
+            for possibility in possibilities:
+                if allergen_food in possibilities[possibility]:
+                    possibilities[possibility].remove(allergen_food)
+        changed = True
+        while changed:
+            changed = False
+            for possibility in possibilities:
+                if len(possibilities[possibility]) == 1:
+                    changed = True
+                    allergen = possibilities[possibility].pop()
+                    dangerous[possibility] = allergen
+                    allergen_foods.add(allergen)
+            for allergen_food in allergen_foods:
+                for possibility in possibilities:
+                    if allergen_food in possibilities[possibility]:
+                        possibilities[possibility].remove(allergen_food)
+
+        non_allergens = set(self.ingredients) - set(allergen_foods)
+        i = 0
+        for food in self.foods:
+            i += len(set(food).intersection(non_allergens))
+        print(dangerous)
+        result = ''
+        for name in sorted(dangerous):
+            if len(result) != 0:
+                result += ','
+            result += dangerous[name]
+        print(result)
+        return str(i)
+
+    def part2(self):
+        print(self.allergens)
+        return ''
---
+++ ac2020/days/Day22.py
@@ -0,0 +1,74 @@
+from collections import deque
+
+from ac2020.days import AbstractDay
+
+
+def game(deck1, deck2):
+    hand1 = list(deck1)
+    hand2 = list(deck2)
+
+    log = set()
+
+    while len(hand1) != 0 and len(hand2) != 0:
+        hashed = tuple(hand1 + hand2)
+        if hashed in log:
+            return hand1, []
+        log.add(hashed)
+        card1 = hand1.pop(0)
+        card2 = hand2.pop(0)
+        if len(hand1) >= card1 and len(hand2) >= card2:
+            sub = game(list(hand1)[:card1], list(hand2)[:card2])
+            if len(sub[0]) > 0:
+                hand1.append(card1)
+                hand1.append(card2)
+            else:
+                hand2.append(card2)
+                hand2.append(card1)
+        else:
+            if card1 >= card2:
+                hand1.append(card1)
+                hand1.append(card2)
+            else:
+                hand2.append(card2)
+                hand2.append(card1)
+    return hand1, hand2
+
+
+class Day22(AbstractDay.AbstractDay):
+
+    def _set_input(self, input_: str):
+        self.entries = {}
+        self.player1 = []
+        self.player2 = []
+        if len(input_) != 0:
+            split = input_.split('\n\n')
+            self.player1 = list(map(int, (split[0].split('\n')[1:])))
+            self.player2 = list(map(int, (split[1].split('\n')[1:])))
+
+    def part1(self) -> str:
+        hand1 = deque(self.player1)
+        hand2 = deque(self.player2)
+        while len(hand1) != 0 and len(hand2) != 0:
+            card1 = hand1.popleft()
+            card2 = hand2.popleft()
+            if card1 > card2:
+                hand1.append(card1)
+                hand1.append(card2)
+            else:
+                hand2.append(card2)
+                hand2.append(card1)
+
+        winner_hand = list(hand2)
+        if len(hand1) > 0:
+            winner_hand = list(hand1)
+
+        return str(sum([(i+1) * x for i, x in enumerate(winner_hand[::-1])]))
+
+    def part2(self) -> str:
+        hands = game(list(self.player1), list(self.player2))
+
+        winner_hand = list(hands[1])
+        if len(hands[0]) > 0:
+            winner_hand = list(hands[0])
+
+        return str(sum([(i + 1) * x for i, x in enumerate(winner_hand[::-1])]))
